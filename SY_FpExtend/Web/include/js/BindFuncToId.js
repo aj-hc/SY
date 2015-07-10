41e5a2b18dacfc836a56df4ebb805a1f442dc35e
alone <zy0803wyl@163.com>
2015-07-10 21:49:05 +0800

    本地更改

---
+++ SY_FpExtend/Web/include/js/BindFuncToId.js
@@ -0,0 +1,19 @@
+﻿//初始化绑定事件
+$(function () {
+    loginOut();
+    btnGet();
+})
+
+
+//注销按钮
+function loginOut() {
+    $("#loginOut").click(function (e) {
+        alert("点击了");
+    });
+}
+//点击查询数据
+function btnGet() {
+    $("#btnGet").click(function (e) {
+        querybycode();
+    });
+}
